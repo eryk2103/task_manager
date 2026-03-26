c36fd71c1039707423ad06ad113f804003d98731
eryk <eryk9903@gmail.com>
2026-03-26 21:08:40 +0100

    add routes to nav

--- web_app/src/app/app.routes.ts
+++ web_app/src/app/app.routes.ts
@@ -1,8 +1,15 @@
 import { Routes } from '@angular/router';
 import { ProjectsPage } from './projects/projects-page/projects-page.component';
 import { NewProjectPage } from './projects/new-project-page/new-project-page.component';
+import { DashboardPage } from './dashboard/dashboard-page/dashboard-page.component';
+import { TasksPage } from './tasks/tasks-page/tasks-page.component';
+import { SettingsPage } from './settings/settings-page/settings-page.component';
 
 export const routes: Routes = [
+    {
+        path: '',
+        component: DashboardPage
+    },
     {
         path: 'projects',
         component: ProjectsPage,
@@ -10,5 +17,13 @@ export const routes: Routes = [
     {
         path: 'projects/new',
         component: NewProjectPage
+    },
+    {
+        path: 'tasks',
+        component: TasksPage
+    },
+    {
+        path: 'settings',
+        component: SettingsPage
     }
 ];
---
+++ web_app/src/app/tasks/tasks-page/tasks-page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tasks-page',
+  imports: [],
+  templateUrl: './tasks-page.component.html',
+  styleUrl: './tasks-page.component.css',
+})
+export class TasksPage {}
