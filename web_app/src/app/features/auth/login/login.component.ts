62b5f1d7a7dc8fccca897308f28d8f67760ca110
eryk <eryk9903@gmail.com>
2026-04-03 16:44:00 +0200

    fix login alert rendering

--- web_app/src/app/features/auth/login/login.component.ts
+++ web_app/src/app/features/auth/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
 import { AuthService } from '../auth.service';
@@ -7,14 +7,15 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatButton } from '@angular/material/button';
 import { Alert } from "../../../shared/alert/alert.component";
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [ReactiveFormsModule, RouterLink, MatFormFieldModule, MatInputModule, MatIconModule, MatButton, Alert],
+  imports: [ReactiveFormsModule, RouterLink, MatFormFieldModule, MatInputModule, MatIconModule, MatButton, Alert, AsyncPipe],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
-export class Login {
+export class Login implements OnInit {
   authService = inject(AuthService);
   router = inject(Router);
 
@@ -22,6 +23,14 @@ export class Login {
     email: new FormControl('', [Validators.required, Validators.email]),
     password: new FormControl('', [Validators.required])
   });
+
+  isLoggedIn = signal<boolean>(false);
+
+  ngOnInit() {
+    this.authService.checkMe().subscribe({
+      next: res => this.isLoggedIn.set(res)
+    });
+  }
 
   get email() {
     return this.form.get('email');
